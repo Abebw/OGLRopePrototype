f2e88e092fa5987b830f4b301d4e54c4ba897bc2
Abe BW <AbeBarthWerb@gmail.com>
2015-03-05 14:15:38 -0500

    changed teh chart from a bar chart to a scatter plot

--- index_files/renderscript.js
+++ index_files/renderscript.js
@@ -5,49 +5,63 @@ console.log("draw chart being defined");
 function drawChart() {
     console.log("draw chart running");
     var data = new google.visualization.DataTable();
-    data.addColumn('string', 'Weight Rating');
-    data.addColumn('number', 'Sterling Fusion Nano test');
-    data.addColumn('number', 'Petzl Fuse');
-    data.addColumn('number', 'Mammut Infinity');
-    data.addColumn('number', 'Mammut Revelation');
-    data.addColumn('number', 'Sterling Marathon Pro');
-    data.addColumn('number', 'Maxim Pinnacle');
-    data.addColumn('number', 'Sterling Evolution Velocity');
-    data.addColumn('number', 'BlueWater Lightning Pro');
-    data.addColumn('number', 'BlueWater Pulse');
-    data.addColumn('number', 'Beal Edlinger');
-    data.addColumn('number', 'Petzl Nomad');
-    data.addColumn('number', 'Mammut Tusk');
-    data.addColumn('number', 'BlueWater Eliminator');
-    data.addColumn('number', 'Maxim Glider');
-    data.addColumn('number', 'Maxim Equinox');
+    data.addColumn('string', 'Rope');
+    data.addColumn('number', 'rope id');
+    data.addColumn('number', 'Weight (kg)');
+   
 
-    data.addRows(1);
-    data.setValue(0, 0, '');
-    data.setValue(0, 1, 9);
-    data.setValue(0, 2, 8);
-    data.setValue(0, 3, 8);
-    data.setValue(0, 4, 8);
-    data.setValue(0, 5, 7);
-    data.setValue(0, 6, 7);
-    data.setValue(0, 7, 7);
-    data.setValue(0, 8, 7);
-    data.setValue(0, 9, 7);
-    data.setValue(0, 10, 6);
-    data.setValue(0, 11, 6);
-    data.setValue(0, 12, 6);
-    data.setValue(0, 13, 6);
-    data.setValue(0, 14, 5);
-    data.setValue(0, 15, 5);
+    data.addRows([
+	["Mammut Infinity",1,2.9  ],
+	["Mammut Infinity",1,3.48  ],
+	["Mammut Infinity",1,4.06  ],
+	["Mammut Infinity",1,4.64  ],
+	["Sterling Fusion Nano",2,2.65  ],
+	["Sterling Fusion Nano",2,3.18  ],
+	["Sterling Fusion Nano",2,3.71  ],
+	["Sterling Fusion Nano",2,4.24  ],
+	["Petzl Fuse",3,3.36  ],
+	["Petzl Fuse",3,3.92  ],
+	["Mammut Revelation",4,3.3  ],
+	["Mammut Revelation",4,3.85  ],
+	["BlueWater Pulse",5,3.72  ],
+	["BlueWater Pulse",5,4.03  ],
+	["BlueWater Pulse",5,4.34  ],
+	["BlueWater Lightning Pro",6,3.66  ],
+	["BlueWater Lightning Pro",6,4.27  ],
+	["Sterling Evolution Velocity",7,3.1  ],
+	["Sterling Evolution Velocity",7,3.72  ],
+	["Sterling Evolution Velocity",7,4.34  ],
+	["Sterling Evolution Velocity",7,4.96  ],
+	["Sterling Marathon Pro",8,3.15  ],
+	["Sterling Marathon Pro",8,3.78  ],
+	["Sterling Marathon Pro",8,4.41  ],
+	["Sterling Marathon Pro",8,5.04  ],
+	["Mammut Tusk",9,3.84  ],
+	["Mammut Tusk",9,4.48  ],
+	["Petzl Nomad",10,3.78  ],
+	["Petzl Nomad",10,4.41  ],
+	["BlueWater Eliminator",11,3.84  ],
+	["BlueWater Eliminator",11,4.48  ],
+	["Maxim Pinnacle",12,3.66  ],
+	["Maxim Pinnacle",12,4.27  ],
+	["Maxim Glider",13,3.96  ],
+	["Maxim Glider",13,4.62  ],
+	["Maxim Equinox",14,3.96  ],
+	["Beal Edlinger",15,3.25  ],
+	["Beal Edlinger",15,3.9  ],
+	["Beal Edlinger",15,4.55  ],
+	["Beal Edlinger",15,5.2  ]
+    ]);
+
 		
     var urls = [''
-, '/Climbing-Rope-Reviews/Sterling-Fusion-Nano', '/Climbing-Rope-Reviews/Petzl-Fuse', '/Climbing-Rope-Reviews/Mammut-Infinity', '/Climbing-Rope-Reviews/Mammut-Revelation', '/Climbing-Rope-Reviews/Sterling-Marathon-Pro', '/Climbing-Rope-Reviews/Maxim-Pinnacle', '/Climbing-Rope-Reviews/Sterling-Evolution-Velocity', '/Climbing-Rope-Reviews/BlueWater-Lightning-Pro', '/Climbing-Rope-Reviews/BlueWater-Pulse', '/Climbing-Rope-Reviews/Beal-Edlinger', '/Climbing-Rope-Reviews/Petzl-Nomad', '/Climbing-Rope-Reviews/Mammut-Tusk', '/Climbing-Rope-Reviews/BlueWater-Eliminator', '/Climbing-Rope-Reviews/Maxim-Glider', '/Climbing-Rope-Reviews/Maxim-Equinox'		];        
+, '/Climbing-Rope-Reviews/Sterling-Fusion-Nano', '/Climbing-Rope-Reviews/Petzl-Fuse', '/Climbing-Rope-Reviews/Mammut-Infinity', '/Climbing-Rope-Reviews/Mammut-Revelation', '/Climbing-Rope-Reviews/Sterling-Marathon-Pro', '/Climbing-Rope-Reviews/Maxim-Pinnacle', '/Climbing-Rope-Reviews/Sterling-Evolution-Velocity', '/Climbing-Rope-Reviews/BlueWater-Lightning-Pro', '/Climbing-Rope-Reviews/BlueWater-Pulse', '/Climbing-Rope-Reviews/Beal-Edlinger', '/Climbing-Rope-Reviews/Petzl-Nomad', '/Climbing-Rope-Reviews/Mammut-Tusk', '/Climbing-Rope-Reviews/BlueWater-Eliminator', '/Climbing-Rope-Reviews/Maxim-Glider', '/Climbing-Rope-Reviews/Maxim-Equinox'	];        
 
-    var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+    var chart = new google.charts.Scatter(document.getElementById('chart_div'));
     chart.draw(data, {width: 920, height: 600, colors: ['#37799B', '#A8462D', '#0A5880', '#A39594', '#B99555', '#394736', '#DC991C', '#BBAD92', '#BCB3A2', '#5C584C', '#051014', '#D5D5D5', '#0F0E5F', '#2D2B41', '#696B46'],							chartArea: {left:45,top:40, width: 550,height:500},
 							legendTextStyle: {color:'#666666'},
 							title: 'Weight',
-							hAxis: {title: 'Weight Rating (0-10 score where 10 is best)', 
+							hAxis: {title: 'Weight Rating (in kilograms)', 
 							titleTextStyle: {color: '#5c5c5c'},
 							titlePosition: 'out', minValue: 5, maxValue: 9},
 							vAxis: {title: '', 
@@ -57,9 +71,8 @@ function drawChart() {
 							is3D: true
 						}
 					);
-		
-    var formatter = new google.visualization.NumberFormat({pattern: "Score: # of '10'"});
-		
+/*
+    var formatter = new google.visualization.NumberFormat({pattern: "Score: # of '10'"});		
     formatter.format(data, 1);
     formatter.format(data, 2);
     formatter.format(data, 3);
@@ -75,6 +88,7 @@ function drawChart() {
     formatter.format(data, 13);
     formatter.format(data, 14);
     formatter.format(data, 15);
+*/
     google.visualization.events.addListener(chart, 'select', selectHandler);
 
     function selectHandler() {
